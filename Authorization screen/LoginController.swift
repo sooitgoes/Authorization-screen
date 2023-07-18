47b7c046ebc6c234efec5efdbf0851d01f3ee2df
Mikhail Laty <laty.mischa@gmail.com>
2023-07-18 20:27:45 +0300

    Configure bind method and add action for button

--- Authorization screen/LoginController.swift
+++ Authorization screen/LoginController.swift
@@ -10,21 +10,38 @@ import UIKit
 class LoginController: UIViewController {
     var viewModel = ViewModel()
 
+    // MARK: - UI Elements
     private var loginView: LoginView? {
         guard isViewLoaded else { return nil }
         return view as? LoginView
     }
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view = LoginView()
         bindViewModel()
+        buttonAction()
     }
 
+    // MARK: - Setup
     private func bindViewModel() {
-
+        viewModel.statusText.bind { text in
+            DispatchQueue.main.async {
+                self.loginView?.statusLabel.text = text
+            }   
+        }
     }
 
+    private func buttonAction() {
+        loginView?.loginButton.addTarget(self, action: #selector(add), for: .touchUpInside)
+    }
 
+    // MARK: - Action
+   @objc func add() {
+       let login = loginView?.loginField.text
+       let password = loginView?.passwordField.text
+       viewModel.buttonPressed(login: login ?? "", password: password ?? "")
+    }
 }
 
